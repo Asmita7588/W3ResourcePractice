f37ce2483d774f0f3371882dd96822355f0d66a1
Asmita Girhepunje <girhepunjeasmita25@gmail.com>
2025-04-10 12:26:03 +0530

    implemented basic programs without using loop

---
+++ LogicalPracticeProgram.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicalPractice
+{
+    internal class LogicalPracticeProgram
+    {
+        public static void PrintNumFrom(int startNum, int endNum)
+        {
+            
+            if (startNum <= endNum)
+            {
+                Console.WriteLine(startNum);
+                PrintNumFrom(startNum + 1, endNum);
+            }
+        }
+
+        //Sum of Digits Without Loop
+        public static int SumOfDigit(int num) {                 //13467  = 21
+
+            if (num == 0)
+            {
+                return 0; 
+            }
+
+            return (num % 10) + SumOfDigit(num / 10);
+        }
+
+        //Check for Palindrome Without Loop or String Reverse Function
+
+        public static bool IsPalindromeString(string str) {
+
+            if (str.Length <= 1)
+            {
+                return true;
+            }
+            else
+            {
+
+                if (str[0] == str[str.Length - 1])
+                {
+                    return IsPalindromeString(str.Substring(1, str.Length - 2));
+                }
+                else
+                {
+                    return false;
+                }
+
+
+            }
+        }
+
+
+        public static string ReverseString(string str ) { 
+
+            int start = 0;
+            int end = str.Length - 1;
+
+            //StringBuilder sb = new StringBuilder();
+
+            if (string.IsNullOrEmpty(str) )
+            {
+                //sb.Append(str[end]);
+                //end--;
+                //start++;
+                return str;
+                
+            }
+            else {
+                return ReverseString(str.Substring(1)) + str[0];
+
+            }
+
+
+           
+        }
+
+        //FizzBuzz Without Using Loop or If-Else
+       // Print 1 to 100 where:
+
+       //If divisible by 3: print "Fizz"
+
+     //If divisible by 5: print "Buzz"
+
+      //If both: "FizzBuzz"
+
+
+        public static void FizzBuzzProgram (int num)
+        {
+            if (num > 100)
+            {
+                return; 
+            }
+
+            string FizzBuzzResult = "";
+            FizzBuzzResult += (num % 3 == 0 ? "Fizz" : "");
+
+            FizzBuzzResult += (num % 5 == 0 ? "Buzz" : "");
+
+            Console.WriteLine(string.IsNullOrEmpty(FizzBuzzResult) ? num.ToString() : FizzBuzzResult);
+
+            FizzBuzzProgram(num + 1);
+        }
+
+
+        //Find If a Number is a Power of Two Without Using Loop 
+        // Example: 8 → true, 6 → false
+
+        public static bool checkPowerOfTwoNum(int num) {
+            if (num <= 0)
+            {
+                return false; 
+            }
+            return (num & (num - 1)) == 0;  // if result 0000 then return true
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace LogicalPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("number from 1 to 100 : ");
+            LogicalPracticeProgram.PrintNumFrom(1, 100);
+
+            int num = 13467;
+            Console.WriteLine($"sum of digit of {num}");
+            int sumResult = LogicalPracticeProgram.SumOfDigit(num);
+
+            Console.WriteLine($"Sum =" + sumResult);
+
+            string palindromStr = "nayan";
+
+            bool isPalindrom = LogicalPracticeProgram.IsPalindromeString(palindromStr);
+
+            if (isPalindrom) {
+                Console.WriteLine($"{palindromStr} is palindrome");
+            }
+            else
+            {
+                Console.WriteLine($"{palindromStr} is not palindrome");
+            }
+
+
+
+            string str = "hello";
+            string reverseResult = LogicalPracticeProgram.ReverseString(str);
+
+            Console.WriteLine("Reverse result = " +reverseResult);
+
+
+            LogicalPracticeProgram.FizzBuzzProgram(1);
+            int num1 = 8;
+           bool result  = LogicalPracticeProgram.checkPowerOfTwoNum(num1);
+            if (result)
+            {
+                Console.WriteLine(num1 + " power of two");
+            }
+            else
+            {
+                Console.WriteLine(num1 + " not power of two");
+            }
+
+
+        }
+
+    }
+}
