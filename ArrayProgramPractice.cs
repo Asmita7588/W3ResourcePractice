fe7514450ca4d87c53179fa96036a2391bd0624c
Asmita Girhepunje <girhepunjeasmita25@gmail.com>
2025-04-12 00:52:08 +0530

    implement basic problem statements on array and string

---
+++ ArrayProgramPractice.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace LogicalPractice
+{
+    internal class ArrayProgramPractice
+    {
+        //Write a C# Sharp program in to count duplicate elements in an array.
+
+
+        public static int CountDuplicateArrayElement(int[] array)
+        {
+            int count = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i +1; j < array.Length; j++) { 
+
+                    if (array[i] == array[j])
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+
+        // Write a C# Sharp program to read the age of a candidate and determine whether it is eligible for casting his/her own vote.
+        //Test Data : 21
+
+        public static int CheckEligibleCandidate(string DOB) { 
+
+            DateTime dob = DateTime.Parse(DOB);
+
+            int dobYear = dob.Year;
+
+            int currentYear = DateTime.Now.Year;
+
+            int  age = currentYear - dobYear;
+
+            if (dob > dob.AddYears(-age))
+            {
+                age--;
+            }
+            return age; 
+        }
+
+        //calculate special char seplarately
+
+        public static void CaluclateSpecialChar()
+        {
+            string input = "W3resource.com Is very good website.";
+
+            int  alphabetCount = 0;
+
+            int digitCount = 0;
+
+            int specialCharCount = 0;
+
+            foreach (char ch in  input )
+            {
+                if (char.IsLetter(ch))
+                {
+                    alphabetCount++;
+
+                }
+                 else if (char.IsDigit(ch))
+                {
+                    digitCount++;
+                }
+                else if (char.IsWhiteSpace(ch) || !char.IsWhiteSpace(ch)) 
+                {
+                    specialCharCount++;
+                }
+            }
+
+            Console.WriteLine("\n Alphabets count in the string is : " + alphabetCount);
+
+            Console.WriteLine("Digits in the string   " + digitCount);
+
+            Console.WriteLine(" Special characters in the string is : " + specialCharCount);
+        }
+
+
+        // copy string and count 
+
+       
+        public static void CopyString()
+        {
+            string str1 = "This is first copied string";
+
+           
+            string copystr = string.Copy(str1); 
+
+            int length = str1.Length;
+
+            Console.WriteLine(" First string is : " + str1);
+            Console.WriteLine("Second copied string is : " + copystr);
+            Console.WriteLine("count of characters copied : " + length);
+        }
+
+
+        //another way to copy
+
+        public static void CopyStringProgram()
+        {
+            string str1 = "This is first copied string";
+
+            string emptyStr = "";
+
+            for (int i = 0; i < str1.Length; i++) {
+                emptyStr += str1[i];
+            }
+            Console.WriteLine("original string :" + str1);
+            Console.WriteLine("Copied string " + emptyStr);
+        }
+
+        // Write a C# Sharp program to find the maximum number of characters in a string.
+
+        public static void FinfHighestFrequencyChar()
+        {
+            string str = "this is book for reading";
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace LogicalPractice
 {
@@ -6,47 +7,76 @@ namespace LogicalPractice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("number from 1 to 100 : ");
-            LogicalPracticeProgram.PrintNumFrom(1, 100);
+            // Console.WriteLine("number from 1 to 100 : ");
+            // LogicalPracticeProgram.PrintNumFrom(1, 100);
 
-            int num = 13467;
-            Console.WriteLine($"sum of digit of {num}");
-            int sumResult = LogicalPracticeProgram.SumOfDigit(num);
+            // int num = 13467;
+            // Console.WriteLine($"sum of digit of {num}");
+            // int sumResult = LogicalPracticeProgram.SumOfDigit(num);
 
-            Console.WriteLine($"Sum =" + sumResult);
+            // Console.WriteLine($"Sum =" + sumResult);
 
-            string palindromStr = "nayan";
+            // string palindromStr = "nayan";
 
-            bool isPalindrom = LogicalPracticeProgram.IsPalindromeString(palindromStr);
+            // bool isPalindrom = LogicalPracticeProgram.IsPalindromeString(palindromStr);
 
-            if (isPalindrom) {
-                Console.WriteLine($"{palindromStr} is palindrome");
+            // if (isPalindrom) {
+            //     Console.WriteLine($"{palindromStr} is palindrome");
+            // }
+            // else
+            // {
+            //     Console.WriteLine($"{palindromStr} is not palindrome");
+            // }
+
+
+
+            // string str = "hello";
+            // string reverseResult = LogicalPracticeProgram.ReverseString(str);
+
+            // Console.WriteLine("Reverse result = " +reverseResult);
+
+
+            // LogicalPracticeProgram.FizzBuzzProgram(1);
+            // int num1 = 8;
+            //bool result  = LogicalPracticeProgram.checkPowerOfTwoNum(num1);
+            // if (result)
+            // {
+            //     Console.WriteLine(num1 + " power of two");
+            // }
+            // else
+            // {
+            //     Console.WriteLine(num1 + " not power of two");
+            // }
+
+
+
+           // //Write a C# Sharp program in to count duplicate elements in an array.
+
+           // int[] arr =  { 10, 20, 30, 40, 70, 30, 70, 10 };
+
+           //int count=   ArrayProgramPractice.CountDuplicateArrayElement(arr);
+
+           // Console.WriteLine("Duplicate array element :" +count);
+
+
+            // Write a C# Sharp program to read the age of a candidate and determine whether it is eligible for casting his/her own vote.
+            //Test Data : 21
+
+            int age = ArrayProgramPractice.CheckEligibleCandidate("2001-11-03");
+            Console.WriteLine(age + "age");
+            if (age <= 21) {
+                Console.WriteLine("candidate is eligible");
             }
-            else
-            {
-                Console.WriteLine($"{palindromStr} is not palindrome");
-            }
 
+            //count speical character
+            ArrayProgramPractice.CaluclateSpecialChar();
 
+            //copy string by method 
 
-            string str = "hello";
-            string reverseResult = LogicalPracticeProgram.ReverseString(str);
+            ArrayProgramPractice.CopyString();
 
-            Console.WriteLine("Reverse result = " +reverseResult);
-
-
-            LogicalPracticeProgram.FizzBuzzProgram(1);
-            int num1 = 8;
-           bool result  = LogicalPracticeProgram.checkPowerOfTwoNum(num1);
-            if (result)
-            {
-                Console.WriteLine(num1 + " power of two");
-            }
-            else
-            {
-                Console.WriteLine(num1 + " not power of two");
-            }
-
+            //copy string another way 
+            ArrayProgramPractice.CopyStringProgram();
 
         }
 
